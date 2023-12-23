bc0c5c95bfea4cebda81362441e8bf871a2b9f1b
Shaikh-Danish <shaikhsaalim327@gmail.com>
2023-12-23 18:07:58 +0530

    comment cell position logic

--- src/app/components/Cells.tsx
+++ src/app/components/Cells.tsx
@@ -1,30 +1,59 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
-import { cells } from "../class/cell-structure";
-import styles from "@/app/styles/Cell.module.css";
+import { cells } from "@/app/class/cell-structure";
+import styles from "@/app/styles/Cells.module.css";
 
 function Cell() {
-  const col = useRef<number>(2);
-  const row = useRef<number>(6);
+  // const [cellPosition, setCellPosition] = useState({ row: 6, col: 1 });
+  // const [breakType, setBreakType] = useState("hr");
+  // const cellPosition = useRef<{ row: number; col: number }>({ row: 6, col: 1 });
+  // const col = useRef<number>(2);
+  // const row = useRef<number>(6);
 
-  const brek = useRef<string>("hor");
+  // console.log(cellPosition.current)
+
+  // const brek = useRef<string>("hr");
 
   return (
     <div>
       {cells.map((cell, i) => {
-        if (cell.break === "hor" || brek.current === "hor") {
-          brek.current = "hor";
-        } else if (cell.break === "ver" || brek.current === "ver") {
-          brek.current = "ver";
-        }
+        // if (
+        //   cell.break === "hr" ||
+        //   (cell.break !== "vr" && breakType === "hr")
+        // ) {
+        //   // setCellPosition((prevPosition) => ({
+        //   //   ...prevPosition,
+        //   //   col: prevPosition.col + 1,
+        //   // }));
+        //   // setBreakType("hr");
+        //   // cellPosition.current.col += 1;
+        //   // brek.current = "hr";
+        // } else if (
+        //   cell.break === "vr" ||
+        //   (cell.break !== "hr" && breakType === "vr")
+        // ) {
+        //   if (cell.break === "vr") {
+        //     // cellPosition.current.col += 1;
+        //   }
+
+        //   // setBreakType("vr");
+
+        //   // cellPosition.current.row -= 1;
+        //   // brek.current = "vr";
+        // }
 
         return (
           <div
             key={i}
             className={styles.cell}
-            style={{ gridColumn: col.current }}
+            style={{
+              // gridColumnStart: cellPosition.col,
+              // gridColumnEnd: cellPosition.current.col + 1,
+              // gridRowStart: cellPosition.current.row,
+              backgroundColor: cell.start ?? "white",
+            }}
           ></div>
         );
       })}
