8b06bb1dd384084072f5e9345a18406c66b45796
Shaikh-Danish <shaikhsaalim327@gmail.com>
2023-12-23 16:46:49 +0530

    feat: incomplete cell rendering logic

---
+++ src/app/components/Cells.tsx
@@ -0,0 +1,19 @@
+import React, { useRef } from "react";
+
+import { cells } from "../class/cell-structure";
+import styles from "@/app/styles/Cell.module.css";
+
+function Cell() {
+  const startCol = useRef(1)
+  const startRow = useRef(6)
+
+  return (
+    <div>
+      {cells.map((cell, i) => (
+        <div key={i} className={styles.cell}></div>
+      ))}
+    </div>
+  );
+}
+
+export default Cell;
