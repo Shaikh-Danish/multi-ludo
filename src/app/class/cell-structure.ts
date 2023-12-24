2a6da8de7fc060b5c11e611782efd86705e9d846
Shaikh-Danish <shaikhsaalim327@gmail.com>
2023-12-24 11:50:49 +0530

    cells for player finish zone

--- src/app/class/cell-structure.ts
+++ src/app/class/cell-structure.ts
@@ -1,12 +1,14 @@
-interface Cell {
+export interface CellType {
   safe?: boolean;
   break?: string;
   start?: string;
   col: number;
   row: number;
+  cells?: Cell[];
+  entry?: string;
 }
 
-export const cells: Cell[] = [
+export const cells: CellType[] = [
   { safe: true, start: "yellow", break: "hr", col: 2, row: 7 },
   { safe: false, col: 3, row: 7 },
   { safe: false, col: 4, row: 7 },
@@ -18,7 +20,7 @@ export const cells: Cell[] = [
   { safe: true, col: 7, row: 3 },
   { safe: false, col: 7, row: 2 },
   { safe: false, col: 7, row: 1 },
-  { safe: false, break: "hr", col: 8, row: 1 },
+  { safe: false, break: "hr", entry: "red", col: 8, row: 1, cells: [{ start: "red", row: 2, col: 8 }, { start: "red", row: 3, col: 8 }, {start: "red", row: 4, col: 8}, {start: "red", row: 5, col: 8}, {start: "red", row: 6, col: 8}] },
   { safe: false, col: 9, row: 1 },
   { safe: true, start: "red", break: "hr", col: 9, row: 2 },
   { safe: false, col: 9, row: 3 },
@@ -31,7 +33,7 @@ export const cells: Cell[] = [
   { safe: true, col: 13, row: 7 },
   { safe: false, col: 14, row: 7 },
   { safe: false, col: 15, row: 7 },
-  { safe: false, break: "hr", col: 15, row: 8 },
+  { safe: false, break: "hr", entry: "green", col: 15, row: 8, cells: [{ start: "green", row: 8, col: 14 }, { start: "green", row: 8, col: 13 }, {start: "green", row: 8, col: 12 }, {start: "green", row: 8, col: 11 }, {start: "green", row: 8, col: 10 }] },
   { safe: false, col: 15, row: 9 },
   { safe: true, start: "green", break: "hr", col: 14, row: 9 },
   { safe: false, col: 13, row: 9 },
@@ -44,7 +46,7 @@ export const cells: Cell[] = [
   { safe: true, col: 9, row: 13 },
   { safe: false, col: 9, row: 14 },
   { safe: false, col: 9, row: 15 },
-  { safe: false, break: "hr", col: 8, row: 15 },
+  { safe: false, break: "hr", entry: "blue", col: 8, row: 15, cells: [{ start: "blue", col: 8, row: 14 }, { start: "blue", col: 8, row: 13 }, {start: "blue", col: 8, row: 12 }, {start: "blue", col: 8, row: 11 }, {start: "blue", col: 8, row: 10 }] },
   { safe: false, col: 7, row: 15 },
   { safe: true, start: "blue", break: "hr", col: 7, row: 14 },
   { safe: false, col: 7, row: 13 },
@@ -57,6 +59,6 @@ export const cells: Cell[] = [
   { safe: true, col: 3, row: 9 },
   { safe: false, col: 2, row: 9 },
   { safe: false, col: 1, row: 9 },
-  { safe: false, break: "hr", col: 1, row: 8 },
+  { safe: false, break: "hr", entry: "yellow", col: 1, row: 8, cells: [{ start: "yellow", row: 8, col: 2 }, { start: "yellow", row: 8, col: 3 }, {start: "yellow", row: 8, col: 4 }, {start: "yellow", row: 8, col: 5 }, {start: "yellow", row: 8, col: 6 }] },
   { safe: false, col: 1, row: 7 },
 ];
