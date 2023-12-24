c745a9df4b0b7006449e614417e8a3c391567eb6
Shaikh-Danish <shaikhsaalim327@gmail.com>
2023-12-24 13:53:02 +0530

    update: key prop and att removed

--- src/app/components/Cell.tsx
+++ src/app/components/Cell.tsx
@@ -5,14 +5,9 @@ import React from "react";
 import { CellType } from "@/app/class/cell-structure";
 import styles from "@/app/styles/Cells.module.css";
 
-interface CellProps extends CellType {
-	key: string
-}
-
-function Cell({ cell, key }: CellProps) {
+function Cell({ cell }: CellProps) {
 	return (
 	    <div
-	      key={key}
 	      className={`${styles.cell} ${styles[cell.start]}`}
 	      style={{
 	        gridColumnStart: cell.col,
