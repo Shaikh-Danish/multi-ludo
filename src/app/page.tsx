2df681108431bc3896dfcf5bdc6df6846aac0705
Shaikh-Danish <shaikhsaalim327@gmail.com>
2023-12-24 18:17:50 +0530

    feat: turn store provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,27 @@ import House from "@/app/components/House"
 import Cells from "@/app/components/Cells"
 import Dice from "@/app/components/Dice"
 
+import { turnContext } from "@/app/context"
+
+
 export default function Home() {
 
-  const [turn, setTurn] = useState<string>("player-1")
+  const [turn, setTurn] = useState<number>(1)
 
   return (
-    <main>
-      <House color={`yellow`} hlBorder={turn === "player-1" ? "hlBorder" : ""} player="player-1" />
-      <House color={`red`} hlBorder={turn === "player-2" ? "hlBorder" : ""} player="player-2"/>
-      <House color={`blue`} hlBorder={turn === "player-4" ? "hlBorder" : ""} player="player-3"/>
-      <House color={`green`} hlBorder={turn === "player-3" ? "hlBorder" : ""} player="player-4"/>
+    <turnContext.Provider value={{ turn, setTurn }}>
+      <main>
+        <House color={`yellow`} hlBorder={turn === 1 ? "hlBorder" : ""} player="player-1" />
+        <House color={`red`} hlBorder={turn === 2 ? "hlBorder" : ""} player="player-2"/>
+        <House color={`blue`} hlBorder={turn === 4 ? "hlBorder" : ""} player="player-3"/>
+        <House color={`green`} hlBorder={turn === 3 ? "hlBorder" : ""} player="player-4"/>
 
-      <div className="home">
-        <Dice />
-      </div>
+        <div className="home">
+          <Dice />
+        </div>
 
-      <Cells />
-    </main>
+        <Cells />
+      </main>
+    </turnContext.Provider>
   )
 }
