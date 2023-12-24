618dbe0977467308a3d1e9dca061f3e1554b5647
Shaikh-Danish <shaikhsaalim327@gmail.com>
2023-12-24 08:46:46 +0530

    removed logic for rendering cells

--- src/app/components/Cells.tsx
+++ src/app/components/Cells.tsx
@@ -6,58 +6,24 @@ import { cells } from "@/app/class/cell-structure";
 import styles from "@/app/styles/Cells.module.css";
 
 function Cell() {
-  // const [cellPosition, setCellPosition] = useState({ row: 6, col: 1 });
-  // const [breakType, setBreakType] = useState("hr");
-  // const cellPosition = useRef<{ row: number; col: number }>({ row: 6, col: 1 });
-  // const col = useRef<number>(2);
-  // const row = useRef<number>(6);
-
-  // console.log(cellPosition.current)
-
-  // const brek = useRef<string>("hr");
-
   return (
-    <div>
+    <>
       {cells.map((cell, i) => {
-        // if (
-        //   cell.break === "hr" ||
-        //   (cell.break !== "vr" && breakType === "hr")
-        // ) {
-        //   // setCellPosition((prevPosition) => ({
-        //   //   ...prevPosition,
-        //   //   col: prevPosition.col + 1,
-        //   // }));
-        //   // setBreakType("hr");
-        //   // cellPosition.current.col += 1;
-        //   // brek.current = "hr";
-        // } else if (
-        //   cell.break === "vr" ||
-        //   (cell.break !== "hr" && breakType === "vr")
-        // ) {
-        //   if (cell.break === "vr") {
-        //     // cellPosition.current.col += 1;
-        //   }
-
-        //   // setBreakType("vr");
-
-        //   // cellPosition.current.row -= 1;
-        //   // brek.current = "vr";
-        // }
-
         return (
           <div
             key={i}
             className={styles.cell}
             style={{
-              // gridColumnStart: cellPosition.col,
-              // gridColumnEnd: cellPosition.current.col + 1,
-              // gridRowStart: cellPosition.current.row,
+              gridColumnStart: cell.col,
+              gridColumnEnd: cell.col,
+              gridRowStart: cell.row,
+              gridRowEnd: cell.row,
               backgroundColor: cell.start ?? "white",
             }}
           ></div>
         );
       })}
-    </div>
+    </>
   );
 }
 
