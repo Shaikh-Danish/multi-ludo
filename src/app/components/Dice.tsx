4468aa33d3ab08b343d7a5bd9d5263ecee58e3d3
Shaikh-Danish <shaikhsaalim327@gmail.com>
2023-12-24 18:18:40 +0530

    feat: move player turns
    incomplete should highlight pieces after that move piece that pass to other player

--- src/app/components/Dice.tsx
+++ src/app/components/Dice.tsx
@@ -1,14 +1,20 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 
 import styles from "@/app/styles/Dice.module.css"
 
+import { turnContext } from "@/app/context"
 
 function Dice() {
 	const [diceNumber, setDiceNumber] = useState<number>(1)
+	const { turn, setTurn } = useContext(turnContext)
 
 	const rollDice = (): void => {
 		const roll = Math.floor(Math.random() * 6) + 1;
 		setDiceNumber(roll)
+
+		if (roll < 6) {
+			//setTurn(getNextPlayer(turn))
+		}
 	}
 
 	return (
@@ -20,6 +26,11 @@ function Dice() {
 
 function Dots() {
 	return <div className={styles.dot}></div>
+}
+
+function getNextPlayer(currentPlayer: number): number {
+	const totalPlayers = 4
+	return (currentPlayer % totalPlayers) + 1;
 }
 
 export default Dice
